54fa21a75f0bd77853931be962613af98c132525
sunlifeng808 <161953183+sunlifeng808@users.noreply.github.com>
2025-01-18 13:12:36 +0800

    eventsystem初步

---
+++ Assets/Script/mevent.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class mevent
+{
+    public bool isfinished;
+    public string name;
+    public string summary;
+}
