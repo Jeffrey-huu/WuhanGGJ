0d36d95c11b021c815449ac4df53773c96b4f584
Roadinforest <142576293+Roadinforest@users.noreply.github.com>
2025-01-18 17:43:02 +0800

    添加Emotion Bar的控制器

---
+++ Assets/Script/EmotionBar.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmotionBar : MonoBehaviour
+{
+    Transform tr;
+    static public EmotionBar instance;
+
+    public int emotionValue;
+
+    void Awake()
+    {
+        instance = this;
+        tr = transform;
+        emotionValue=50;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateScale();
+    }
+
+    //情绪最大值：100，最小值：0
+    public void AddEmotion(int value)
+    {
+        emotionValue = Mathf.Clamp(emotionValue + value,0,100);
+    }
+
+    public void DecreaseEmotion(int value)
+    {
+        emotionValue = Mathf.Clamp(emotionValue - value,0,100);
+    }
+
+    private void UpdateScale()
+    {
+        float scale = emotionValue / 13.80f;
+        transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(scale, 1, 1), Time.deltaTime * 5);
+    }
+}
