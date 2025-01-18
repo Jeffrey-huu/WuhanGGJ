433bc1c3f5df398936fee6551b89787fc33bef36
Jeffrey-huu <50811411+Jeffrey-huu@users.noreply.github.com>
2025-01-19 00:56:30 +0800

    增加数字跳动效果

--- Assets/Script/MainUIManager.cs
+++ Assets/Script/MainUIManager.cs
@@ -2,19 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class MainUIManager : MonoBehaviour
 {
     public BubbleController bubbleController;
     public TextMeshProUGUI bubbleValue;
-    public void UpdateValue()
+
+    private float currentDisplayedValue;
+    [SerializeField] private float lerpSpeed = 5f;  // 插值的平滑速度
+
+    private void Start()
     {
-        bubbleValue.text = bubbleController.currentAsset.ToString();
+        // 初始化显示值为当前资产
+        currentDisplayedValue = bubbleController.currentAsset;
     }
 
     void Update()
     {
-        UpdateValue();
+        // 动态插值更新显示值
+        currentDisplayedValue = Mathf.Lerp(currentDisplayedValue, bubbleController.currentAsset, Time.deltaTime * lerpSpeed);
+
+        // 更新UI显示
+        bubbleValue.text = Mathf.RoundToInt(currentDisplayedValue).ToString();
     }
 }
