96cb274c71427c36d7b9bf80673d6354805934af
sunlifeng808 <161953183+sunlifeng808@users.noreply.github.com>
2025-01-18 22:35:12 +0800

    字体

--- Assets/Script/BubbleController.cs
+++ Assets/Script/BubbleController.cs
@@ -20,7 +20,10 @@ public class BubbleController : MonoBehaviour, IPointerDownHandler, IPointerUpHa
     private float pressStartTime = 0f;
     [SerializeField] private float maxPressDuration = 3.0f;
 
-    public int currentAsset = 0;
+    public int currentAsset => personAsset + marketAsset;
+    public int personAsset = 0;
+    public int marketAsset = 0;
+
     private float lerpSpeed = 0.1f;
     private int additiveAsset;
 
@@ -70,14 +73,14 @@ public class BubbleController : MonoBehaviour, IPointerDownHandler, IPointerUpHa
     //Control Signal - 1
     public void AddAsset(int num)
     {
-        currentAsset += num;
+        personAsset += num;
         CheckValid();
     }
 
     //Control Signal - 2
     public void DecreaseAsset(int num)
     {
-        currentAsset -= num;
+        personAsset -= num;
         CheckValid();
     }
 
